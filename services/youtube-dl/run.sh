fab8256f6df0c6f0d872926f7f9d63022018d199
luandro <luandro@gmail.com>
2020-07-05 15:24:30 -0300

    working youtube-dl

--- services/youtube-dl/run.sh
+++ services/youtube-dl/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl -L https://github.com/Tzahi12345/YoutubeDL-Material/archive/master.zip -o youtubedl-material-docker.zip
 unzip youtubedl-material-docker.zip
-cd YoutubeDL-Material-master/backend
+docker-compose up -d
+
