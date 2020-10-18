2fd6171b089629023976f82bf38b51946e043d10
luandro <luandro@gmail.com>
2020-10-18 19:16:28 -0300

    use docker-compose linux/server

--- scripts/install/install_docker_aarch64.sh
+++ scripts/install/install_docker_aarch64.sh
@@ -7,5 +7,8 @@ sudo gpasswd -a $USER docker && sudo chmod 664 /var/run/docker.sock
 # Install dependencies and docker-compose
 sudo apt update -y
 sudo apt install -y exfat-fuse ntfs-3g
-sudo apt install -y vim git libffi-dev libssl-dev gcc python3 python3-pip python3-setuptools
-sudo pip3 install docker-compose
+sudo apt install -y vim git libffi-dev libssl-dev gcc docker-compose
+# Alternative running from container
+sudo curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
