a4fe5c35cd79d3d9daac2e9a373bcd0a7bad42fa
luandro <luandro@gmail.com>
2021-01-01 13:55:54 -0300

    youtube-dl usando imagem do docker

--- services/youtube-dl/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -L https://github.com/Tzahi12345/YoutubeDL-Material/archive/master.zip -o youtubedl-material-docker.zip
-unzip youtubedl-material-docker.zip
-mv Dockerfile YoutubeDL-Material-master/
-docker-compose up -d
-
